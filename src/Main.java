f63f0df09bdf78d8c487e5a7edcc2d91fe1f5fb8
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2024-06-26 01:39:56 +0100

    Testing
    JSON parsing tests

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
 import ayohee.json.JSON;
 
-import java.nio.file.Path;
+import java.io.EOFException;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        System.out.println(JSON.Parse(Path.of("test files", "1.json")).toString());
-        System.out.println(JSON.Parse(Path.of("test files", "2.json")).toString());
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0){
+            System.exit(3);
+        }
+
+        try{
+            JSON.Parse(Paths.get(args[0]));
+            System.exit(0);
+        }
+        catch (IllegalStateException | EOFException e) {
+            System.exit(1);
+        }
+        catch (IOException e){
+            System.exit(2);
+        }
     }
 }
